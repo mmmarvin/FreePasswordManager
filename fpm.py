6f0fbd31ac46d16c27993ced8aa054bb63416fc5
mmmarvin <43771610+mmmarvin@users.noreply.github.com>
2021-03-25 00:42:26 -0600

    fixed license

--- fpm.py
+++ fpm.py
@@ -12,7 +12,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+# along with FreePasswordManager.  If not, see <https://www.gnu.org/licenses/>.
 ########
 from PyQt5.QtWidgets import *
 import detail.mainwindow
